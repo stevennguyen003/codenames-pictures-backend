ab28027b02794d9436347bf599a0c7152e3766cc
stevennguyen003 <96093087+stevennguyen003@users.noreply.github.com>
2024-12-16 15:37:30 -0500

    Reverted bug

--- SocketEvents/socketEvents.js
+++ SocketEvents/socketEvents.js
@@ -77,7 +77,6 @@ const socketEvents = (io, redisClient) => {
 
             // First time users get defaulted to spectator, need to update room
             if (!existingUserInRoom) {
-                room.users.push({ id: socket.id });
                 room.spectators.push({ nickname, id: socket.id, role: 'spectator' });
                 await createOrUpdateRoom(roomName, room);
             }
